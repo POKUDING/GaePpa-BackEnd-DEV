d30ddc85024557417435067717399344ca5d5417
POKUDING <yoseb95@gmail.com>
2024-11-06 15:10:07 +0900

    init commit

---
+++ src/main/java/com/sparta/ggaeppa/GgaePpaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.ggaeppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgaePpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgaePpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/ggaeppa/GgaePpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.ggaeppa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgaePpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
