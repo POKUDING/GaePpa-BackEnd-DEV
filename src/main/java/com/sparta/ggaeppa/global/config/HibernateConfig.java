a62b9fc733d42df4cd4d633179808e874052dbae
ksw4060 <gokorea1214@kakao.com>
2024-11-08 12:16:45 +0900

    [Configuration] Hibernate6Module 사용, WebMvcConfig 중에서 리소스파일의 헨들러설정. /images 경로의 리소스파일을 바로 찾아준다

--- src/main/java/com/sparta/ggaeppa/global/config/HibernateConfig.java
+++ src/main/java/com/sparta/ggaeppa/global/config/HibernateConfig.java
@@ -1,2 +1,13 @@
-package com.sparta.ggaeppa.global.config;public class HibernateConfig {
+package com.sparta.ggaeppa.global.config;
+
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HibernateConfig {
+    @Bean
+    public Hibernate6Module hibernate6Module() {
+        return new Hibernate6Module();
+    }
 }
