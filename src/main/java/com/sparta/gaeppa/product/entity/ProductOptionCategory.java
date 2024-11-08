8a5ea343b85cbcc25d5bd9441e274785b8b541be
POKUDING <yoseb95@gmail.com>
2024-11-08 16:38:30 +0900

    feat: product option category Entity 개발 RelatedTo: #18

---
+++ src/main/java/com/sparta/gaeppa/product/entity/ProductOptionCategory.java
@@ -0,0 +1,34 @@
+package com.sparta.gaeppa.product.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_product_option_categories")
+public class ProductOptionCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_option_category_id")
+    private UUID id;
+
+    @Column(name = "product_option_category_name")
+    private String name;
+
+    @Column(name = "product_option_max_limits")
+    private Integer maxLimits;
+
+//    @ManyToOne
+//    @JoinColumn(name = "product_id", nullable = false)
+//    private Product product;
+
+//    @OneToMany(mappedBy = "optionCategory")
+//    private List<ProductOption> options;
+
+    // getters and setters
+}
