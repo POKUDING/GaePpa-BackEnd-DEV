656074df00e26eff2e27354c66075d6a08c9873a
DISNOTACAY <yeonjh915@gmail.com>
2024-11-16 02:12:30 +0900

    Bug : Order POST 저장 오류 - 주문 저장시 persist 에러 발생
    Payment Test 필요

---
+++ src/main/java/com/sparta/gaeppa/payment/repository/PaymentsRepository.java
@@ -0,0 +1,17 @@
+package com.sparta.gaeppa.payment.repository;
+
+import com.sparta.gaeppa.order.entity.Orders;
+import com.sparta.gaeppa.payment.entity.Payments;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentsRepository extends JpaRepository<Payments, UUID> {
+    Payments findPaymentByOrder(Orders order);
+
+    @Query(value = "SELECT p FROM Payments p JOIN Orders po where po.memberId = :memberId")
+    List<Payments> findAllPaymentsByMemberId(UUID memberId);
+}
