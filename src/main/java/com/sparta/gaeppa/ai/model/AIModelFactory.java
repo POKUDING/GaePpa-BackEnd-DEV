24c2fda04133d1adc1dfaece5be3c5bfe66a1443
ksw4060 <gokorea1214@kakao.com>
2024-11-18 02:11:38 +0900

    feat(ai): Update AI-related components (Controller, Model, Repository, Service)

--- src/main/java/com/sparta/gaeppa/ai/model/AIModelFactory.java
+++ src/main/java/com/sparta/gaeppa/ai/model/AIModelFactory.java
@@ -4,7 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 public class AIModelFactory {
 
