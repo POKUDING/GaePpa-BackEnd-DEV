ae55ea655a1a06708649426fc2edcb51a38465b3
김성우 <127410562+ksw4060@users.noreply.github.com>
2024-11-18 10:41:07 +0900

    Update StoreAdminController.java

--- src/main/java/com/sparta/gaeppa/store/controller/StoreAdminController.java
+++ src/main/java/com/sparta/gaeppa/store/controller/StoreAdminController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/admin.stores")
+@RequestMapping("/v1/admin/stores")
 @RequiredArgsConstructor
 @Slf4j
 public class StoreAdminController {
