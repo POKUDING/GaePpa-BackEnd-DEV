c2d31bda06ae1be6360e41d548eb916f479b771b
POKUDING <yoseb95@gmail.com>
2024-11-18 15:47:35 +0900

    Feat: member_id를 통해 Store를 가져올 수 있는 메소드 추가 RelatedTo: #55

--- src/main/java/com/sparta/gaeppa/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/gaeppa/store/repository/StoreRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.gaeppa.store.repository;
 
 import com.sparta.gaeppa.store.entity.Store;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,6 @@ public interface StoreRepository extends JpaRepository<Store, UUID> {
     Store findByMemberMemberId(UUID memberId);
 
     Store findByStoreName(String storeName);
+
+    Optional<Store> getStoreByMember_MemberId(UUID memberId);
 }
