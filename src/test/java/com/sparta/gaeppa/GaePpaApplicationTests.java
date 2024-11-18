50df6e78ed970554aa358278534c5592d927bba9
POKUDING <yoseb95@gmail.com>
2024-11-18 15:46:57 +0900

    chore: 사용하지 않는 테스트 코드 비활성화 RelatedTo: #55

--- src/test/java/com/sparta/gaeppa/GaePpaApplicationTests.java
+++ src/test/java/com/sparta/gaeppa/GaePpaApplicationTests.java
@@ -1,19 +1,17 @@
 package com.sparta.gaeppa;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GaePpaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
-    @Test
-    @DisplayName("ddl-create 확인")
-    void test() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//    @Test
+//    @DisplayName("ddl-create 확인")
+//    void test() {
+//    }
 
 }
