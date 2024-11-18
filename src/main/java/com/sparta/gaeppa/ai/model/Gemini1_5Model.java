dc95b1e5ab145a94da617b830b45b5b956744d0f
김성우 <127410562+ksw4060@users.noreply.github.com>
2024-11-18 10:51:25 +0900

    Update Gemini1_5Model.java

--- src/main/java/com/sparta/gaeppa/ai/model/Gemini1_5Model.java
+++ src/main/java/com/sparta/gaeppa/ai/model/Gemini1_5Model.java
@@ -12,38 +12,38 @@ import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class Gemini1_5Model implements IAIModel {
-//
-//    public static final String MODEL_NAME = "Gemini1.5";
-//
-//    private static final String URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=";
-//
-//    private final RestTemplate restTemplate;
-//
-//    @Value("${google.ai.key}")
-//    private String apiKey;
-//
-//    @Override
-//    public String getName() {
-//        return MODEL_NAME;
-//    }
-//
-//    @Override
-//    public String processTextRequest(String requestText) {
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//
-//        RequestEntity<Gemini1_5TextRequestDto> requestEntity = new RequestEntity<Gemini1_5TextRequestDto>(
-//                Gemini1_5TextRequestDto.builder().text(requestText).build(), headers, HttpMethod.POST,
-//                URI.create(URL + apiKey));
-//
-//        return Objects.requireNonNull(restTemplate.exchange(requestEntity, Gemini1_5ResponseDto.class).getBody())
-//                .getCandidates().getFirst()
-//                .getContent().getParts().getFirst().getText();
-//
-//    }
-//}
+
+@Component
+@RequiredArgsConstructor
+public class Gemini1_5Model implements IAIModel {
+
+   public static final String MODEL_NAME = "Gemini1.5";
+
+   private static final String URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=";
+
+   private final RestTemplate restTemplate;
+
+   @Value("${google.ai.key}")
+   private String apiKey;
+
+   @Override
+   public String getName() {
+       return MODEL_NAME;
+   }
+
+   @Override
+   public String processTextRequest(String requestText) {
+
+       HttpHeaders headers = new HttpHeaders();
+       headers.setContentType(MediaType.APPLICATION_JSON);
+
+       RequestEntity<Gemini1_5TextRequestDto> requestEntity = new RequestEntity<Gemini1_5TextRequestDto>(
+               Gemini1_5TextRequestDto.builder().text(requestText).build(), headers, HttpMethod.POST,
+               URI.create(URL + apiKey));
+
+       return Objects.requireNonNull(restTemplate.exchange(requestEntity, Gemini1_5ResponseDto.class).getBody())
+               .getCandidates().getFirst()
+               .getContent().getParts().getFirst().getText();
+
+   }
+}
